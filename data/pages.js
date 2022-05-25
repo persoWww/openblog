a663ba56f34e40c96d620cabf5301d6fe4e2d8f3
Adrien-Ray <a.raymond@codeur.online>
2022-05-25 09:52:18 +0200

    page download

--- data/pages.js
+++ data/pages.js
@@ -14,10 +14,25 @@ const pages = [{
     </ul>`,
 },
 {
+    title: 'Contact',
+    id: 'ds5213',
+    date: '2022-05-25',
+    author: 'dev',
+    content: `<p>vous pouvez me contacter via ce <a href="https://portfolio.accesdenied.net/pages/apropos.php">formulaire de contact</a>, en précisant que votre message est en rapport avec frontblog.</p>`,
+},
+{
+    title: 'download',
+    id: 'hg5421',
+    date: '2022-05-25',
+    author: 'dev',
+    content: `<h2><a href="https://github.com/persoWww/frontblog/archive/refs/heads/version_1.5.zip">download version 1.5</a></h2>
+    <h2><a href="https://github.com/persoWww/frontblog/archive/refs/heads/frontblog.accesdenied.net.zip">download this documentation</a></h2>`,
+},
+{
     title: 'accueil', // le title accueil est un mot clef pour désigner le contenu de la page d'accueil
     id: 'ds2037p4',
     date: '2022-05-22',
     author: 'dev',
-    content: ``,
+    content: `<p>cette documentation est téléchargeable via <a href="https://github.com/persoWww/frontblog/archive/refs/heads/frontblog.accesdenied.net.zip">ce lien</a> ou via git: <code class="language-markup">git clone https://github.com/persoWww/frontblog.git && git checkout frontblog.accesdenied.net</code>.</p>`,
 },
 ];
